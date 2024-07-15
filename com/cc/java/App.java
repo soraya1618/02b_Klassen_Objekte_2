0168cb19cd78b65400716f81bc6d57e649f74311
Peter <schmidt.multimedia@gmail.com>
2024-07-15 12:32:37 +0200

    01 / Felder 1. Iteration

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -13,11 +13,17 @@ public class App {
         output("Blick von aussen: " + cat);
         output("Blick von innen: " + cat.getInstanceVariable());
 
+        output(cat.name);
+        output(cat.furColor);
+        // Explizite Typumwandlung
+        output(Integer.toString(cat.age));
+
+
         System.out.println("----------------------------");
 
-        Cat cat2 = new Cat();
-        output("Blick von aussen: " + cat2);
-        output("Blick von innen: " + cat2.getInstanceVariable());
+        // Cat cat2 = new Cat();
+        // output("Blick von aussen: " + cat2);
+        // output("Blick von innen: " + cat2.getInstanceVariable());
 
     }
 
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,7 +1,17 @@
 package com.cc.java;
 
-public class Cat 
-{
+public class Cat {
+  
+    // Eigenschaften / properties
+    // Felder / fields / attributes
+
+    // pass by reference
+    String name = "Grizabella";
+    String furColor = "white";
+    
+    // pass by value
+    int age = 29;
+    
     // Rückgabetyp -- Cat
     public Cat getInstanceVariable(){
         return this;
